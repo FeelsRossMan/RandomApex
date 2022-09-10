02c8d78ffd2091a3b1ce566a6c809232361f1e30
Evan Lahey <25727163+FeelsRossMan@users.noreply.github.com>
2022-09-09 17:09:49 -0700

    Re organized the viewmodel so that it's in the homescreen package

--- app/src/main/java/com/evanross/randomapex/HomeFragment.kt
+++ app/src/main/java/com/evanross/randomapex/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.evanross.randomapex.databinding.FragmentHomeBinding
+import com.evanross.randomapex.homeScreen.HomeViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,6 +16,8 @@ import com.evanross.randomapex.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    private val rollModel: HomeViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +29,12 @@ class HomeFragment : Fragment() {
     ): View? {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val nameObserver = Observer<Int> {
+                newChar -> binding.characterRolled.text = newChar.toString()
+        }
+        rollModel.character.observe(viewLifecycleOwner, nameObserver)
         return binding.root
+
 
     }
 
@@ -33,7 +42,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.randomButton.setOnClickListener {
-            findNavController().navigate(R.id.action_HomeFragment_to_RandomFragment)
+            //findNavController().navigate(R.id.action_HomeFragment_to_RandomFragment)
+            rollModel.rollCharacter()
         }
     }
 
