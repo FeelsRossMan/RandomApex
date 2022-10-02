00c20bd918d4c27fda6a280d973d900b2ab25ee5
Evan Lahey <25727163+FeelsRossMan@users.noreply.github.com>
2022-10-02 11:19:19 -0700

    Added gun icons

--- app/src/main/java/com/evanross/randomapex/model/ApexGun.kt
+++ app/src/main/java/com/evanross/randomapex/model/ApexGun.kt
@@ -1,5 +1,47 @@
 package com.evanross.randomapex.model
 
-class ApexGun {
+import androidx.annotation.DrawableRes
 
-}
+data class ApexGun(
+    @DrawableRes val iconArtImageId: Int,
+    val name: String,
+    val id: Int,
+    val primaryAmmoType: String,
+    val gunClass: String,
+    val secondaryAmmoType: String? = null,
+    val carePackageGun: Boolean = false
+)
+/*
+
+3030repeater,
+a13sentry,
+alternator,
+bocek,
+car,
+charge_rifle,
+devotion,
+eva8,
+flatline,
+g7_scout,
+havoc,
+hemlock,
+kraber,
+longbow,
+lstar,
+mastiff,
+mozambique,
+p2020,
+peacekeeper,
+prowler,
+r99,
+r301,
+rampage,
+re45,
+sentinal,
+spitfire,
+triple_take,
+volt,
+wingman,
+
+
+ */
